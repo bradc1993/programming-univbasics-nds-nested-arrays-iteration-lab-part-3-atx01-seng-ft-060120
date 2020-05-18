a28d03511657113cc59094520ac0387d21ba0d2b
Brad Carter <bradc1993@gmail.com>
2020-05-18 23:04:35 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,4 +7,12 @@ outer_count = 0
 while outer_count < src.length do
   inner_count = 0
   while inner_count < src[outer_count].length do
+    final_string += src[outer_count][inner_count]
+    inner_count += 1
+  end
+  outer_count += 1
+end
+final_string
+end
+  
     
